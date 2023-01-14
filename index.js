472b8ebfdfa50f4d1f99f9cc6cb7a1c3b2e680b6
Алексей <alexeybudai@gmail.com>
2023-01-15 00:13:44 +0300

    add bgMenu and slider

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+function menuOnClick() {
+    document.getElementById("menu-bar").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+    document.getElementById("menu-bg").classList.toggle("change-bg");
+}
 
+let offset = 0;
+const sliderLine = document.querySelector('.slider_line');
+
+document.querySelector('.next').addEventListener('click', function(){
+    offset = offset + 70;
+    if (offset > 170) {
+        offset = 0;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    offset = offset - 70;
+    if (offset < 0) {
+        offset = 140;
+    }
+    sliderLine.style.left = -offset + 'px';
+});
